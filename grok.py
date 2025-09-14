aa7a552b34bb63468d3d770b2ee59601ceda7cc3
Afghanistantest <162014924+Afghanistantest@users.noreply.github.com>
2025-09-14 20:29:01 +0430

    Update grok.py

--- grok.py
+++ grok.py
@@ -1,9 +1,1 @@
-# d.py
-import grokv8        # اسم ماژولی که فایل .so بهش مربوطه
-
-# اگر ماژول تابعی به نام foo داشته باشه:
-if hasattr(grokv8, "foo"):
-    print("calling c.foo():", grokv8.foo())
-else:
-    # لیست اعضا رو چاپ کن تا ببینی چه چیزی صادر شده
-    print("module members:", dir(grokv8))
+import grokv8
